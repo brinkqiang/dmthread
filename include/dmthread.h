f820e7d292a6bc027075efdda3f9cfe6cfe4adee
brink.qiang <brink.qiang@gmail.com>
2018-10-16 23:55:06 +0800

    add

--- include/dmthread.h
+++ include/dmthread.h
@@ -1,5 +1,5 @@
 
-// Copyright (c) 2018 brinkqiang (brink.qiang@gmail.com)
+// Copyright (c) 2018 brinkqiang
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -24,6 +24,111 @@
 
 #include "dmos.h"
 
-int DMAPI DMTHREAD_MAIN( int argc, char* argv[] );
+#include <thread>
+#include <memory>
+
+class IDMThread {
+  public:
+    virtual ~IDMThread() {}
+    virtual void ThrdProc() = 0;
+    virtual void Terminate() = 0;
+};
+
+class IDMThreadCtrl {
+  public:
+    virtual ~IDMThreadCtrl() {}
+    virtual void Resume() = 0;
+    virtual void Suspend() = 0;
+    virtual void Stop() = 0;
+    virtual bool Kill( unsigned int dwExitCode ) = 0;
+    virtual bool WaitFor() = 0;
+    virtual std::thread::id GetThreadID() = 0;
+    virtual IDMThread* GetThread() = 0;
+    virtual void Release() = 0;
+};
+
+class CDMThreadCtrl : public IDMThreadCtrl {
+  public:
+    CDMThreadCtrl() {
+        m_bIsStop = true;
+        m_bNeedWaitFor = true;
+        m_poThread = NULL;
+    }
+
+    virtual ~CDMThreadCtrl() {
+    }
+
+  public:
+    virtual void Resume( void ) {
+
+    }
+
+    virtual void Suspend() {
+
+    }
+
+    virtual void Stop( void ) {
+        m_poThread->Terminate();
+    }
+
+    virtual bool Kill( unsigned int dwExitCode ) {
+        return true;
+    }
+
+    virtual bool WaitFor() {
+        if ( m_oThread->joinable() ) {
+            m_oThread->join();
+        }
+
+        return true;
+    }
+
+    virtual void Release( void ) {
+        delete this;
+    }
+
+    virtual std::thread::id GetThreadID( void ) {
+        return m_oThread->get_id();
+    }
+
+    virtual IDMThread* GetThread( void ) {
+        return m_poThread;
+    }
+
+    static void* StaticThreadFunc( void* arg ) {
+        CDMThreadCtrl* poCtrl = ( CDMThreadCtrl* )arg;
+        poCtrl->m_bIsStop = false;
+
+        poCtrl->m_poThread->ThrdProc();
+        return 0;
+    }
+
+    bool Start( IDMThread* poThread, bool bNeedWaitFor = true ) {
+        m_poThread = poThread;
+
+        m_bNeedWaitFor = bNeedWaitFor;
+
+        std::unique_ptr<std::thread> oThread( new std::thread( StaticThreadFunc,
+                                              this ) );
+
+        m_oThread = std::move( oThread );
+
+        if ( !bNeedWaitFor ) {
+            m_oThread->detach();
+        }
+
+        return true;
+    }
+
+  protected:
+    volatile bool   m_bIsStop;
+    volatile bool   m_bNeedWaitFor;
+    std::unique_ptr<std::thread> m_oThread;
+    IDMThread*      m_poThread;
+};
+
+inline IDMThreadCtrl* CreateThreadCtrl() {
+    return new CDMThreadCtrl;
+}
 
 #endif // __DMTHREAD_H_INCLUDE__
--- test/dmthreadtest.cpp
+++ test/dmthreadtest.cpp
@@ -1,8 +1,116 @@
 
+#include "dmutil.h"
+#include "dmsingleton.h"
 #include "dmthread.h"
-#include "dmformat.h"
+#include "dmconsole.h"
+#include "dmtypes.h"
+#include "dmlock.h"
 
-int main( int argc, char* argv[] ) {
-    std::cout << fmt::format("{0} {1}", "hello world! ", DMTHREAD_MAIN(argc, argv));
+#include <mutex>
+
+#define MAX_THREADS 10
+#define MAX_LOCK 1000000;
+
+uint64_t gCount = 0;
+std::mutex mutexLock;
+CLock gLock;
+
+class CMain :
+    public IDMConsoleSink,
+    public IDMThread,
+    public CDMThreadCtrl,
+    public TSingleton<CMain>
+{
+    friend class TSingleton<CMain>;
+public:
+
+    virtual void ThrdProc() {
+
+        {
+            std::list<std::thread*> threadlist;
+
+            //≤‚ ‘mutex  
+            printf("testing mutex...\n");
+
+            clock_t start = clock();
+            for (int i = 0; i < MAX_THREADS; ++i)
+            {
+                threadlist.push_back(new std::thread([] {
+                    for (int j = 0; j < 1000000; ++j)
+                    {
+                        mutexLock.lock();
+                        gCount += j;
+                        mutexLock.unlock();
+                    }
+                }));
+            }
+
+            for (auto in : threadlist)
+            {
+                in->join();
+            }
+            clock_t finish = clock();
+            printf("result:%llu\n", gCount);
+            printf("cost:%dms\n", finish - start);
+        }
+        {
+            std::list<std::thread*> threadlist;
+
+            //≤‚ ‘mutex  
+            printf("testing CLock...\n");
+
+            clock_t start = clock();
+            for (int i = 0; i < MAX_THREADS; ++i)
+            {
+                threadlist.push_back(new std::thread([] {
+
+                    for (int j = 0; j < 1000000; ++j)
+                    {
+                        gLock.lock();
+                        gCount += j;
+                        gLock.unlock();
+                    }
+ 
+                }));
+            }
+
+            for (auto in : threadlist)
+            {
+                in->join();
+            }
+            clock_t finish = clock();
+            printf("result:%llu\n", gCount);
+            printf("cost:%dms\n", finish - start);
+        }
+    }
+
+    virtual void Terminate() {
+        m_bStop = true;
+    }
+
+    virtual void OnCloseEvent() {
+        Stop();
+    }
+private:
+    CMain()
+        : m_bStop(false) {
+        HDMConsoleMgr::Instance()->SetHandlerHook(this);
+    }
+
+    virtual ~CMain() {
+
+    }
+
+private:
+    bool __Run() {
+        return false;
+    }
+private:
+    volatile bool   m_bStop;
+};
+
+int main(int argc, char* argv[]) {
+    CMain::Instance()->Start(CMain::Instance());
+    CMain::Instance()->WaitFor();
     return 0;
 }
