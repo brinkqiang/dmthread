b1c9007276769b04013fa4f7da2e400309ef33ba
brinkqiang <brink.qiang@gmail.com>
2024-06-05 00:44:41 +0800

    add

--- include/dmthreadpool.h
+++ include/dmthreadpool.h
@@ -27,7 +27,7 @@ private:
     // synchronization
     std::mutex queue_mutex;
     std::condition_variable condition;
-    bool stop;
+    std::atomic_bool stop;
 };
  
 // the constructor just launches some amount of workers
